7e48e942d093bbe9516a2ea6885900579af29d6a
thaddeuskkr <thaddeuskkr@gmail.com>
2022-01-27 12:50:38 +0800

    Fix timestamps

--- commands/timestamp.js
+++ commands/timestamp.js
@@ -1,17 +1,24 @@
 const { getVoiceConnection } = require('@discordjs/voice');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'timestamp',
     description: 'Sends you the current UNIX timestamp to make it easier to find recordings.',
     options: [],
-    run: async ({ ctx }) => {
+    run: async ({ ctx, avatar }) => {
         const connection = getVoiceConnection(ctx.guild.id);
         if (!connection) {
             await ctx.reply({ content: 'I\'m not currently recording.', ephemeral: true });
             return; 
         }
         const timestamp = Date.now();
-        await ctx.user.send(`**Current UNIX timestamp:** \`${timestamp}\`\nSaved **<t:${timestamp}:R>** (hover for exact time)`);
+        const embed = new MessageEmbed()
+            .setAuthor({ name: ctx.user.tag, iconURL: ctx.user.displayAvatarURL({ dynamic: true, size: 4096 }) })
+            .setColor('PURPLE')
+            .setDescription(`**Current UNIX timestamp:** \`${timestamp}\``)
+            .setTimestamp()
+            .setFooter({ name: 'Mochi - mochibot.me | Saved at', iconURL: avatar });
+        await ctx.user.send({ embeds: [embed] });
         await ctx.reply({ content: `**Current UNIX timestamp:** \`${timestamp}\`\n**Check your DMs!**`, ephemeral: true });
     }
 };
